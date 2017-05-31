8b9a367814ea49a215d3cbdf84511efb704ce037
thomasfanto <thomas.fanto@consid.se>
2017-05-31 14:12:25 +0200

    .

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/BuildAssetDeployment.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/BuildAssetDeployment.java
@@ -1,11 +1,21 @@
 package eu.europa.ec.fisheries.uvms.asset.arquillian;
 
+import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+
+import java.io.File;
+
 /**
  * Created by andreasw on 2017-02-13.
  */
-//@ArquillianSuiteDeployment
+@ArquillianSuiteDeployment
 public abstract class BuildAssetDeployment {
-/*
+
     @Deployment(name = "normal", order = 1)
     public static Archive<?> createDeployment() {
 
@@ -17,40 +27,21 @@ public abstract class BuildAssetDeployment {
         // So that Arquillian can invoke test class through its servlet test runner
         WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war");
 
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.constant");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.entity");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.dao");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.mapper");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.model.exception");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.util");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.dto");
-        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mobileterminal.search");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.constant");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.entity");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.dao");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.mapper");
+        testWar.addPackages(true, "eu.europa.ec.fisheries.uvms.util");
         testWar.addPackages(true, "com.tocea.easycoverage.framework.api");
         
-        testWar.addPackages(true,"eu.europa.ec.fisheries.uvms.mobileterminal.model");
+        testWar.addPackages(true,"eu.europa.ec.fisheries.uvms.asset.model");
         
         testWar.addPackages(true,"eu.europa.ec.fisheries.uvms.mobileterminal.arquillian");
         testWar.addPackages(true,"eu.europa.ec.fisheries.uvms.mobileterminal.arquillian.bean");
         
         testWar.addPackages(true, "eu.europa.ec.fisheries.schema");
         testWar.addClass(TransactionalTests.class);
-        testWar.addClass(TerminalDao.class);
-        testWar.addClass(TerminalDaoBean.class);
-        testWar.addClass(MobileTerminal.class);
-        testWar.addClass(MobileTerminalTypeEnum.class);
-        testWar.addClass(MobileTerminalPluginDao.class);
-        testWar.addClass(MobileTerminalPluginDaoBean.class);
-     
-        testWar.addClass(PollDaoBean.class);
-        testWar.addClass(PollSearchKeyValue.class);
-        testWar.addClass(PollSearchMapper.class);
-
-        testWar.addClass(PollProgramDao.class);
-        testWar.addClass(PollProgram.class);
-
-
-
+        testWar.addClass(TestMe.class);
 
         testWar.addAsResource("persistence-integration.xml", "META-INF/persistence.xml");
         // Empty beans for EE6 CDI
@@ -61,5 +52,5 @@ public abstract class BuildAssetDeployment {
 
         return testWar;
     }
-*/
+
 }
