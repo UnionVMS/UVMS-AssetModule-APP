80ee47ceaa121ab7d5414c738492185bd1505e8c
Andreas Wallen <andreasw@Andreass-MacBook-Pro.local>
2017-06-20 15:48:33 +0200

    Removed unused code and fixed logging

--- service/src/main/java/eu/europa/ec/fisheries/uvms/asset/service/bean/ConfigServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/asset/service/bean/ConfigServiceBean.java
@@ -19,8 +19,6 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import eu.europa.ec.fisheries.schema.config.types.v1.SettingType;
-import eu.europa.ec.fisheries.uvms.asset.message.consumer.AssetQueueConsumer;
-import eu.europa.ec.fisheries.uvms.asset.message.producer.MessageProducer;
 import eu.europa.ec.fisheries.uvms.asset.model.exception.AssetException;
 import eu.europa.ec.fisheries.uvms.asset.remote.dto.ConfigurationDto;
 import eu.europa.ec.fisheries.uvms.asset.service.ConfigService;
@@ -30,7 +28,6 @@ import eu.europa.ec.fisheries.wsdl.asset.config.ConfigField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.europa.ec.fisheries.uvms.config.constants.ConfigHelper;
 import eu.europa.ec.fisheries.uvms.config.exception.ConfigServiceException;
 import eu.europa.ec.fisheries.uvms.config.service.ParameterService;
 
@@ -39,21 +36,11 @@ public class ConfigServiceBean implements ConfigService {
 	final static Logger LOG = LoggerFactory.getLogger(ConfigServiceBean.class);
 
     @EJB
-    MessageProducer messageProducer;
-
-    @EJB
-    AssetQueueConsumer reciever;
-	
-    @EJB
     ParameterService parameterService;
 
-	//@EJB(lookup = ServiceConstants.DB_ACCESS_CONFIG_DOMAIN_MODEL)
 	@EJB
 	private ConfigDomainModelBean configDomainModel;
 
-    @EJB
-    ConfigHelper configHelper;
-    
 	@Override
 	public List<Config> getConfiguration() throws AssetException {
         LOG.info("Get configuration.");
@@ -74,7 +61,7 @@ public class ConfigServiceBean implements ConfigService {
 			return parameters;
 		}
 		catch (ConfigServiceException e) {
-			LOG.error("[ Error when getting asset parameters from local database. ] {}", e.getMessage());
+			LOG.error("[ Error when getting asset parameters from local database. ] {}", e);
 			throw new AssetException("Couldn't get parameters");
 		}
 	}
