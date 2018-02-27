496e44d55cb7d8550dcbb183f91e58d015bf3ef3
peerik <peter.eriksson@havochvatten.se>
2018-02-27 15:51:21 +0100

    Added more tests

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetSEDaoTest.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetSEDaoTest.java
@@ -13,7 +13,11 @@ package eu.europa.ec.fisheries.uvms.asset.arquillian;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 import java.util.UUID;
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
@@ -183,5 +187,69 @@ public class AssetSEDaoTest extends TransactionalTests {
         AssetSE updatedAsset = assetDao.getAssetById(asset.getId());
         assertThat(updatedAsset.getId(), is(asset.getId()));
         assertThat(updatedAsset.getName(), is(newName));
+    }
+    
+    @Test
+    @OperateOnDeployment("normal")
+    public void getRevisionsForAssetSingleRevisionTest() throws AssetDaoException {
+        AssetSE asset = AssetTestsHelper.createBasicAsset();
+        asset = assetDao.createAsset(asset);
+        
+        List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(asset);
+        
+        assertEquals(1, assetRevisions.size());
+        
+    }
+    
+    @Test
+    @OperateOnDeployment("normal")
+    public void getRevisionsForAssetCheckSizeTest() throws AssetDaoException {
+        AssetSE asset = AssetTestsHelper.createBasicAsset();
+        asset = assetDao.createAsset(asset);
+        
+        String newName1 = "NewName1";
+        asset.setName(newName1);
+        asset = assetDao.updateAsset(asset);
+       
+        List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(asset);
+        
+        assertEquals(2, assetRevisions.size());
+    }
+    
+    @Test
+    @OperateOnDeployment("normal")
+    public void getRevisionsForAssetCompareRevisionsTest() throws AssetDaoException {
+        AssetSE asset = AssetTestsHelper.createBasicAsset();
+        AssetSE assetVersion1 = assetDao.createAsset(asset);
+        
+        String newName1 = "NewName1";
+        assetVersion1.setName(newName1);
+        AssetSE assetVersion2 = assetDao.updateAsset(assetVersion1);
+        
+        String newName2 = "NewName2";
+        assetVersion2.setName(newName2);
+        AssetSE assetVersion3 = assetDao.updateAsset(assetVersion2);
+        
+        assertThat(assetVersion3.getId(), is(notNullValue()));
+        List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(assetVersion3);
+        
+        assertEquals(3, assetRevisions.size());
+     
+        //TODO compare revisions with versions
+    }
+    
+    @Test
+    @OperateOnDeployment("normal")
+    public void getAssetAtDateSingleAssetTest() throws AssetDaoException {
+        AssetSE asset = AssetTestsHelper.createBasicAsset();
+        asset = assetDao.createAsset(asset);
+        
+        AssetSE assetAtDate = assetDao.getAssetAtDate(asset, LocalDateTime.now(ZoneOffset.UTC));
+
+        assertThat(assetAtDate.getId(), is(notNullValue()));
+        
+        assertThat(assetAtDate.getName(), is(asset.getName()));
+        assertThat(assetAtDate.getCfr(), is(asset.getCfr()));
+        assertThat(assetAtDate.getActive(), is(asset.getActive()));
     }
 }
