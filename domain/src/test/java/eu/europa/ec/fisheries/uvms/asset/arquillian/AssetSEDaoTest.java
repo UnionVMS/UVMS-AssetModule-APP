ee9d52d1e0bc0c93315b1cd8391d9b0fbdbd545b
peerik <peter.eriksson@havochvatten.se>
2018-03-01 12:58:16 +0100

    more tests

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetSEDaoTest.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetSEDaoTest.java
@@ -32,6 +32,10 @@ import eu.europa.ec.fisheries.uvms.entity.model.AssetSE;
 @RunWith(Arquillian.class)
 public class AssetSEDaoTest extends TransactionalTests {
 
+    // Envers requires a commit for auditing
+    // Set this to true to clean up test data 
+    private boolean cleanUpDB = true;
+
     @Inject
     AssetSEDao assetDao;
 
@@ -40,22 +44,22 @@ public class AssetSEDaoTest extends TransactionalTests {
     public void createAssetTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         assertThat(asset.getId(), is(notNullValue()));
-        
+
         AssetSE fetchedAsset = assetDao.getAssetById(asset.getId());
-        
+
         assertThat(fetchedAsset.getName(), is(asset.getName()));
         assertThat(fetchedAsset.getCfr(), is(asset.getCfr()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test(expected = AssetDaoException.class)
     @OperateOnDeployment("normal")
     public void createAssetNullInputShouldThrowExceptionTest() throws AssetDaoException {
         assetDao.createAsset(null);
     }
-    
+
     // TODO should test history GUID
     @Ignore
     @Test
@@ -63,16 +67,16 @@ public class AssetSEDaoTest extends TransactionalTests {
     public void createAssetCheckHistoryGuid() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-       
-//        assertThat(asset.getHistoryGuid()), is(notNullValue()));
+
+        // assertThat(asset.getHistoryGuid()), is(notNullValue()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByCfrTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByCfr(asset.getCfr());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -80,7 +84,7 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getCfr(), is(asset.getCfr()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByCfrTestNonExistingCrf() throws AssetDaoException {
@@ -88,13 +92,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         AssetSE fetchedAsset = assetDao.getAssetByCfr(randomCrf);
         assertThat(fetchedAsset, is(nullValue()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByIrcsTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByIrcs(asset.getIrcs());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -102,13 +106,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getIrcs(), is(asset.getIrcs()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByImoTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByImo(asset.getImo());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -116,13 +120,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getImo(), is(asset.getImo()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByMmsiTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByMmsi(asset.getMmsi());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -130,13 +134,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getMmsi(), is(asset.getMmsi()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByIccatTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByIccat(asset.getIccat());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -144,13 +148,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getIccat(), is(asset.getIccat()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByUviTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByUvi(asset.getUvi());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -158,13 +162,13 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getUvi(), is(asset.getUvi()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void getAssetByGfcmTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         AssetSE fetchedAsset = assetDao.getAssetByGfcm(asset.getGfcm());
 
         assertThat(fetchedAsset.getId(), is(asset.getId()));
@@ -172,84 +176,172 @@ public class AssetSEDaoTest extends TransactionalTests {
         assertThat(fetchedAsset.getGfcm(), is(asset.getGfcm()));
         assertThat(fetchedAsset.getActive(), is(asset.getActive()));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
     public void updateAssetTest() throws AssetDaoException {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+
         String newName = "UpdatedName";
         asset.setName(newName);
         asset = assetDao.updateAsset(asset);
         assertThat(asset.getName(), is(newName));
-        
+
         AssetSE updatedAsset = assetDao.getAssetById(asset.getId());
         assertThat(updatedAsset.getId(), is(asset.getId()));
         assertThat(updatedAsset.getName(), is(newName));
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
-    public void getRevisionsForAssetSingleRevisionTest() throws AssetDaoException {
+    public void getAssetListAllTest() throws AssetDaoException {
+        List<AssetSE> assetListBefore = assetDao.getAssetListAll();
+        assetDao.createAsset(AssetTestsHelper.createBasicAsset());
+        List<AssetSE> assetListAfter = assetDao.getAssetListAll();
+
+        assertThat(assetListAfter.size(), is(assetListBefore.size() + 1));
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void getRevisionsForAssetSingleRevisionTest() throws Exception {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+        commit();
+
         List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(asset);
-        
+
         assertEquals(1, assetRevisions.size());
-        
+
+        if (cleanUpDB) {
+            cleanUpDB(asset);
+        }
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
-    public void getRevisionsForAssetCheckSizeTest() throws AssetDaoException {
+    public void getRevisionsForAssetTwoVersionsCheckSizeTest() throws Exception {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+        commit();
+
+        AssetSE fetchedAsset = assetDao.getAssetById(asset.getId());
         String newName1 = "NewName1";
-        asset.setName(newName1);
-        asset = assetDao.updateAsset(asset);
-       
-        List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(asset);
-        
+        fetchedAsset.setName(newName1);
+        assetDao.updateAsset(fetchedAsset);
+        commit();
+
+        List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(fetchedAsset);
+
         assertEquals(2, assetRevisions.size());
+
+        if (cleanUpDB) {
+            cleanUpDB(fetchedAsset);
+        }
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
-    public void getRevisionsForAssetCompareRevisionsTest() throws AssetDaoException {
+    public void getRevisionsForAssetCompareRevisionsTest() throws Exception {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         AssetSE assetVersion1 = assetDao.createAsset(asset);
-        
+        commit();
+
         String newName1 = "NewName1";
         assetVersion1.setName(newName1);
         AssetSE assetVersion2 = assetDao.updateAsset(assetVersion1);
-        
+        commit();
+
         String newName2 = "NewName2";
         assetVersion2.setName(newName2);
         AssetSE assetVersion3 = assetDao.updateAsset(assetVersion2);
-        
+        commit();
+
         assertThat(assetVersion3.getId(), is(notNullValue()));
         List<AssetSE> assetRevisions = assetDao.getRevisionsForAsset(assetVersion3);
-        
+
         assertEquals(3, assetRevisions.size());
-     
-        //TODO compare revisions with versions
+
+        // TODO 
+//        AssetSE rev1 = assetRevisions.get(0);
+//        assertThat(rev1.getName(), is(asset.getName()));
+//        assertThat(rev1.getCfr(), is(asset.getCfr()));
+//        assertThat(rev1.getActive(), is(asset.getActive()));
+//        
+//        AssetSE rev2 = assetRevisions.get(1);
+//        assertThat(rev2.getName(), is(assetVersion1.getName()));
+//        assertThat(rev2.getCfr(), is(assetVersion1.getCfr()));
+//        assertThat(rev2.getActive(), is(assetVersion1.getActive()));
+//        
+//        AssetSE rev3 = assetRevisions.get(2);
+//        assertThat(rev3.getName(), is(assetVersion2.getName()));
+//        assertThat(rev3.getCfr(), is(assetVersion2.getCfr()));
+//        assertThat(rev3.getActive(), is(assetVersion2.getActive()));
+
+        if (cleanUpDB) {
+            cleanUpDB(assetVersion3);
+        }
     }
-    
+
     @Test
     @OperateOnDeployment("normal")
-    public void getAssetAtDateSingleAssetTest() throws AssetDaoException {
+    public void getAssetAtDateSingleAssetTest() throws Exception {
         AssetSE asset = AssetTestsHelper.createBasicAsset();
         asset = assetDao.createAsset(asset);
-        
+        commit();
+
         AssetSE assetAtDate = assetDao.getAssetAtDate(asset, LocalDateTime.now(ZoneOffset.UTC));
 
         assertThat(assetAtDate.getId(), is(notNullValue()));
-        
+
         assertThat(assetAtDate.getName(), is(asset.getName()));
         assertThat(assetAtDate.getCfr(), is(asset.getCfr()));
         assertThat(assetAtDate.getActive(), is(asset.getActive()));
+
+        if (cleanUpDB) {
+            cleanUpDB(asset);
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void getAssetAtDateMultipleAssetsTest() throws Exception {
+        AssetSE asset1 = AssetTestsHelper.createBasicAsset();
+        asset1 = assetDao.createAsset(asset1);
+        String firstName = asset1.getName();
+        LocalDateTime firstDate = LocalDateTime.now(ZoneOffset.UTC);
+        commit();
+
+        String newName = "NewName";
+        asset1.setName(newName);
+        AssetSE asset2 = assetDao.updateAsset(asset1);
+        LocalDateTime secondDate = LocalDateTime.now(ZoneOffset.UTC);
+        commit();
+
+        AssetSE assetAtFirstDate = assetDao.getAssetAtDate(asset2, firstDate);
+        assertThat(assetAtFirstDate.getName(), is(firstName));
+
+        AssetSE assetAtSecondDate = assetDao.getAssetAtDate(asset2, secondDate);
+        assertThat(assetAtSecondDate.getName(), is(newName));
+
+        if (cleanUpDB) {
+            cleanUpDB(asset1);
+        }
+    }
+
+    private void commit() throws Exception {
+        userTransaction.commit();
+        userTransaction.begin();
+    }
+
+    private void cleanUpDB(AssetSE asset) throws Exception {
+        String sql = "delete from assetse where id = '" + asset.getId() + "'";
+        em.createNativeQuery(sql).executeUpdate();
+        sql = "delete from assetse_aud where id = '" + asset.getId() + "'";
+        em.createNativeQuery(sql).executeUpdate();
+        sql = "delete from revinfo where rev not in (select rev from assetse_aud)";
+        em.createNativeQuery(sql).executeUpdate();
+        commit();
     }
 }
