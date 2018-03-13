bb37701ff54cc4b4088073885910891cf555ece3
thomasfanto <thomas.fanto@consid.se>
2018-03-13 10:24:54 +0100

    positive tests for AssetGroup  before changing  it to   SE

--- domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetGroupTestsIT.java
+++ domain/src/test/java/eu/europa/ec/fisheries/uvms/asset/arquillian/AssetGroupTestsIT.java
@@ -1,22 +1,20 @@
 package eu.europa.ec.fisheries.uvms.asset.arquillian;
 
 
-import eu.europa.ec.fisheries.uvms.asset.types.AssetIdTypeEnum;
 import eu.europa.ec.fisheries.uvms.dao.AssetGroupDao;
 import eu.europa.ec.fisheries.uvms.dao.exception.AssetGroupDaoException;
 import eu.europa.ec.fisheries.uvms.entity.assetgroup.AssetGroupEntity;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @RunWith(Arquillian.class)
-public class AssetGroupTestsIT extends TransactionalTests  {
+public class AssetGroupTestsIT extends TransactionalTests {
 
 
     private Random rnd = new Random();
@@ -25,41 +23,163 @@ public class AssetGroupTestsIT extends TransactionalTests  {
     private AssetGroupDao assetGroupDao;
 
 
-
     @Test
     @OperateOnDeployment("normal")
     public void getAssetGroupAll() throws AssetGroupDaoException {
 
+        List<Long> createdList = new ArrayList<>();
+        List<Long> fetchedList = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            AssetGroupEntity createdAssetGroupEntity = createAndStoreAssetGroupEntity("TEST");
+            createdList.add(createdAssetGroupEntity.getId());
+        }
         List<AssetGroupEntity> rs = assetGroupDao.getAssetGroupAll();
+        for (AssetGroupEntity e : rs) {
+            fetchedList.add(e.getId());
+        }
 
-
-        System.out.println(rs);
-
-
+        // the list from db MUST contain our created Id:s
+        Boolean ok = true;
+        for (Long aCreated : createdList) {
+            if(!fetchedList.contains(aCreated)){
+                ok = false;
+                break;
+            }
+        }
+        Assert.assertTrue(ok);
     }
-
-
 
     @Test
     @OperateOnDeployment("normal")
     public void createAssetGroup() throws AssetGroupDaoException {
 
-
-        AssetGroupEntity assetGroupEntity = createAssetGroupEntity();
-
-        AssetGroupEntity createdAssetGroupEntity = assetGroupDao.createAssetGroup(assetGroupEntity);
-
-
+        AssetGroupEntity createdAssetGroupEntity1 = createAndStoreAssetGroupEntity("TEST");
+        Assert.assertTrue(createdAssetGroupEntity1 != null);
     }
 
-    private AssetGroupEntity createAssetGroupEntity() {
+    @Test
+    @OperateOnDeployment("normal")
+    public void getAssetGroupByUser() throws AssetGroupDaoException {
+
+        String user1 = UUID.randomUUID().toString();
+        String user2 = UUID.randomUUID().toString();
+        String user3 = UUID.randomUUID().toString();
+
+        for (int i = 0; i < 3; i++) {
+           createAndStoreAssetGroupEntity(user1);
+        }
+        for (int i = 0; i < 8; i++) {
+            createAndStoreAssetGroupEntity(user2);
+        }
+        for (int i = 0; i < 11; i++) {
+            createAndStoreAssetGroupEntity(user3);
+        }
+
+        List<AssetGroupEntity> listUser1 = assetGroupDao.getAssetGroupByUser(user1);
+        List<AssetGroupEntity> listUser2 = assetGroupDao.getAssetGroupByUser(user2);
+        List<AssetGroupEntity> listUser3 = assetGroupDao.getAssetGroupByUser(user3);
+
+        Assert.assertTrue(listUser1.size() == 3);
+        Assert.assertTrue(listUser2.size() == 8);
+        Assert.assertTrue(listUser3.size() == 11);
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void getAssetGroupByGuid() throws AssetGroupDaoException {
+
+        AssetGroupEntity createdAssetGroupEntity = createAndStoreAssetGroupEntity("TEST");
+        String guid = createdAssetGroupEntity.getGuid();
+        Assert.assertTrue(guid != null);
+
+        AssetGroupEntity fetchedAssetGroupEntity =  assetGroupDao.getAssetGroupByGuid(guid);
+        Assert.assertEquals(guid, fetchedAssetGroupEntity.getGuid());
+    }
+
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void getAssetGroupByGUIDS() throws AssetGroupDaoException {
+
+        List<String> createdList = new ArrayList<>();
+        List<String> fetchedList = new ArrayList<>();
+        List<AssetGroupEntity> fetchedEntityList ;
+        for (int i = 0; i < 5; i++) {
+            AssetGroupEntity createdAssetGroupEntity = createAndStoreAssetGroupEntity("TEST");
+            createdList.add(createdAssetGroupEntity.getGuid());
+        }
+
+        fetchedEntityList = assetGroupDao.getAssetGroupsByGroupGuidList(createdList);
+        for(AssetGroupEntity e : fetchedEntityList){
+            fetchedList.add(e.getGuid());
+        }
+
+        // the list from db MUST contain our created GUIDS:s
+        Boolean ok = true;
+        for (String aCreatedGUID : createdList) {
+            if(!fetchedList.contains(aCreatedGUID)){
+                ok = false;
+                break;
+            }
+        }
+        Assert.assertTrue(ok);
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void deleteAssetGroup() throws AssetGroupDaoException {
+
+        AssetGroupEntity assetGroupEntity = createAndStoreAssetGroupEntity("TEST");
+        String uuid = assetGroupEntity.getGuid();
+        assetGroupDao.deleteAssetGroup(assetGroupEntity);
+
+        try {
+            AssetGroupEntity fetchedGroup = assetGroupDao.getAssetGroupByGuid(uuid);
+            Assert.assertTrue(fetchedGroup == null);
+        }
+        catch(AssetGroupDaoException e){
+            // throws exception when no record found
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void updateAssetGroup() throws AssetGroupDaoException {
+
+        AssetGroupEntity assetGroupEntity = createAndStoreAssetGroupEntity("TEST");
+        String uuid = assetGroupEntity.getGuid();
+
+        assetGroupEntity.setOwner("NEW OWNER");
+        assetGroupDao.updateAssetGroup(assetGroupEntity);
+        em.flush();
+
+            AssetGroupEntity fetchedGroup = assetGroupDao.getAssetGroupByGuid(uuid);
+            Assert.assertTrue(fetchedGroup.getOwner().equalsIgnoreCase("NEW OWNER"));
+    }
+
+
+
+
+
+
+    private AssetGroupEntity createAndStoreAssetGroupEntity(String user) throws AssetGroupDaoException {
+
+        AssetGroupEntity assetGroupEntity = createAssetGroupEntity(user);
+        AssetGroupEntity createdAssetGroupEntity = assetGroupDao.createAssetGroup(assetGroupEntity);
+        return createdAssetGroupEntity;
+    }
+
+
+    private AssetGroupEntity createAssetGroupEntity(String user) {
         AssetGroupEntity ag = new AssetGroupEntity();
 
         ag.setUpdatedBy("test");
         ag.setUpdateTime(new Date(System.currentTimeMillis()));
         ag.setArchived(false);
         ag.setName("Donald the Trumpier Duck");
-        ag.setOwner("Earth");
+        ag.setOwner(user);
         ag.setDynamic(false);
         ag.setGlobal(true);
 
