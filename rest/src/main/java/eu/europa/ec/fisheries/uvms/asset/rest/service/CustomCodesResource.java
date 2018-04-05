fd5530bf939be161c9f337209b1246ecd11bf071
thomasfanto <thomas.fanto@consid.se>
2018-04-05 16:29:58 +0200

    some minor tests mosty of pedagogic character

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/service/CustomCodesResource.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/service/CustomCodesResource.java
@@ -1,11 +1,13 @@
 package eu.europa.ec.fisheries.uvms.asset.rest.service;
 
 
+import eu.europa.ec.fisheries.uvms.asset.domain.entity.Asset;
 import eu.europa.ec.fisheries.uvms.asset.domain.entity.CustomCodes;
 import eu.europa.ec.fisheries.uvms.asset.service.CustomCodesService;
 import eu.europa.ec.fisheries.uvms.rest.security.RequiresFeature;
 import eu.europa.ec.fisheries.uvms.rest.security.UnionVMSFeature;
 import eu.europa.ec.fisheries.wsdl.asset.types.ConfigSearchField;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +21,7 @@ import java.util.List;
 @Path("/customcodes")
 @Stateless
 @RequiresFeature(UnionVMSFeature.viewVesselsAndMobileTerminals)
+@Api(value = "CustomCodes Service")
 public class CustomCodesResource {
 
 
@@ -27,7 +30,36 @@ public class CustomCodesResource {
     @Inject
     private CustomCodesService customCodesSvc;
 
+    @POST
+    @ApiOperation(value = "Create a record", notes = "Create a custom constant code", response = CustomCodes.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Error when createing custom code"),
+            @ApiResponse(code = 200, message = "Success when createing custom code") })
+    @Path("/{constant}/{code}/{description}/{embeddedjson}")
+    @Consumes(value = {MediaType.APPLICATION_JSON})
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    public Response createCustomCode(
+            @ApiParam(value = "constant", required = true)  @PathParam(value="constant") String constant
+            ,@ApiParam(value = "code", required = true)  @PathParam(value="code")  String code
+            ,@ApiParam(value = "description", required = true)  @PathParam(value="description")  String description
+            , @ApiParam(value = "embeddedjson", required = true)  @PathParam(value="embeddedjson") String embeddedjson) {
+        try {
+             CustomCodes customCodes = customCodesSvc.create(constant,code,description,embeddedjson);
+            return Response.ok(customCodes).build();
+        } catch (Exception e) {
+            LOG.error("Error when getting config search fields.");
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
+
+
+
+
     @GET
+    @ApiOperation(value = "Get a list of constants", notes = "Get a list of constants from Custom Code", response = String.class, responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Error when retrieving constant list"),
+            @ApiResponse(code = 200, message = "Constants successfully retrieved") })
     @Path("/")
     @Consumes(value = {MediaType.APPLICATION_JSON})
     @Produces(value = {MediaType.APPLICATION_JSON})
@@ -42,6 +74,10 @@ public class CustomCodesResource {
     }
 
     @GET
+    @ApiOperation(value = "Get a list of codes for given", notes = "Returned as json parse tree in client´´", response = String.class, responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 500, message = "Error when retrieving code list for given constant list"),
+            @ApiResponse(code = 200, message = "Codes for constant  successfully retrieved") })
     @Path("/getcodesforconstant/{constant}")
     @Consumes(value = {MediaType.APPLICATION_JSON})
     @Produces(value = {MediaType.APPLICATION_JSON})
--- rest/src/test/java/eu/europa/ec/fisheries/uvms/asset/rest/service/CustomCodesResourceTest.java
+++ rest/src/test/java/eu/europa/ec/fisheries/uvms/asset/rest/service/CustomCodesResourceTest.java
@@ -2,14 +2,19 @@ package eu.europa.ec.fisheries.uvms.asset.rest.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.europa.ec.fisheries.uvms.asset.domain.entity.CustomCodes;
 import eu.europa.ec.fisheries.uvms.asset.rest.AbstractAssetRestTest;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 
 @RunWith(Arquillian.class)
@@ -71,20 +76,60 @@ public class CustomCodesResourceTest extends AbstractAssetRestTest {
                 System.out.println(description);
                 System.out.println(embeddedJson);
             }
-
-
-
-
-
-
-
-
-
         }
+    }
+
+    @Test
+    @RunAsClient
+    public void createACustomCode() throws IOException {
+
+        String txt = UUID.randomUUID().toString();
+        String createdJson = createACustomCodeHelper(txt);
+        CustomCodes customCodes = MAPPER.readValue(createdJson, CustomCodes.class);
+
+
+        System.out.println("       " + txt);
+        System.out.println(customCodes.getPrimaryKey().getConstant());
+        System.out.println(customCodes.getPrimaryKey().getCode());
+        System.out.println(customCodes.getDescription());
+        System.out.println(customCodes.getJsonstr());
+
+
 
 
     }
-}
+
+
+    private String  createACustomCodeHelper(String txt){
+
+
+        String constant = "CST____" + txt;
+        String code = "CODE___" + txt;
+        String descr = "DESCR_" +txt;
+        String embeddedjson = "EMB____" + txt;
+
+
+
+        Response resp = getWebTarget()
+                .path("customcodes")
+                .path(constant)
+                .path(code)
+                .path(descr)
+                .path(embeddedjson)
+                .request(MediaType.APPLICATION_JSON)
+                .post(null, Response.class);
+        String entity = resp.readEntity(String.class);
+        return entity;
+
+    }
+
+
+
+
+
+
+
+    }
 
 
 
