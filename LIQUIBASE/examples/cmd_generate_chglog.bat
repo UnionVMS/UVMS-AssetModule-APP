438cc3bf20a57e55ce67d1eb0b13c690aa01823c
thomasfanto <thomas.fanto@consid.se>
2018-04-16 12:49:58 +0200

    ensure tablestructure is at latest

--- LIQUIBASE/examples/cmd_generate_chglog.bat
+++ LIQUIBASE/examples/cmd_generate_chglog.bat
@@ -1,2 +1,2 @@
-liquibase --driver=org.postgresql.Driver --classpath=lib\postgresql-42.2.1.jar --changeLogFile=db-changelog-createTables.xml --url="jdbc:postgresql://localhost:15432/db71u" --username=asset --password=asset generateChangeLog
+liquibase --driver=org.postgresql.Driver --classpath=lib\postgresql-42.2.1.jar --changeLogFile=db-changelog-createTables.xml --url="jdbc:postgresql://localhost:25432/db71u" --username=asset --password=asset generateChangeLog
 	  
