728272e32794875652d1a67bf91e945bbc174bad
Jonas E.Mattsson <jonas.mattsson@consid.se>
2018-07-06 09:36:30 +0200

    Moved tests from one class to another.

--- service/src/test/java/eu/europa/fisheries/uvms/tests/mobileterminal/service/arquillian/PollServiceBeanIntTest.java
+++ service/src/test/java/eu/europa/fisheries/uvms/tests/mobileterminal/service/arquillian/PollServiceBeanIntTest.java
@@ -1,16 +1,19 @@
 package eu.europa.fisheries.uvms.tests.mobileterminal.service.arquillian;
 
-import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollAttribute;
-import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollAttributeType;
-import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollRequestType;
-import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.PollResponseType;
+import eu.europa.ec.fisheries.schema.mobileterminal.polltypes.v1.*;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.bean.PollServiceBean;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.dao.PollProgramDaoBean;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.dto.CreatePollResultDto;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.dto.PollDto;
+import eu.europa.ec.fisheries.uvms.mobileterminal.service.dto.PollKey;
+import eu.europa.ec.fisheries.uvms.mobileterminal.service.dto.PollValue;
+import eu.europa.ec.fisheries.uvms.mobileterminal.service.entity.Channel;
+import eu.europa.ec.fisheries.uvms.mobileterminal.service.entity.MobileTerminal;
 import eu.europa.ec.fisheries.uvms.mobileterminal.service.entity.PollProgram;
+import eu.europa.ec.fisheries.uvms.mobileterminal.service.mapper.PollMapper;
 import eu.europa.fisheries.uvms.tests.TransactionalTests;
 import eu.europa.fisheries.uvms.tests.mobileterminal.service.arquillian.helper.TestPollHelper;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -19,8 +22,10 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
-import java.util.Date;
-import java.util.List;
+import javax.ejb.EJBTransactionRolledbackException;
+import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -30,8 +35,8 @@ public class PollServiceBeanIntTest extends TransactionalTests {
     private static final String MESSAGE_PRODUCER_METHODS_FAIL = "MESSAGE_PRODUCER_METHODS_FAIL";
 
 
-    @EJB
-    private PollServiceBean pollService;
+    @Inject
+    private PollServiceBean pollServiceBean;
 
     @EJB
     private TestPollHelper testPollHelper;
@@ -42,12 +47,29 @@ public class PollServiceBeanIntTest extends TransactionalTests {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private Calendar cal = Calendar.getInstance();
+
     @Test
     public void createPoll()  {
         System.setProperty(MESSAGE_PRODUCER_METHODS_FAIL, "false");
         PollRequestType pollRequestType = testPollHelper.createPollRequestType();
-        CreatePollResultDto createPollResultDto = pollService.createPoll(pollRequestType, "TEST");
+        CreatePollResultDto createPollResultDto = pollServiceBean.createPoll(pollRequestType, "TEST");
         assertNotNull(createPollResultDto);
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void createPoll_FromMPSBIT() throws Exception {   //MPSBIT = Mapped Poll Service Bean Int Test, a test class for a, now removed, middle layer
+
+        PollRequestType pollRequestType = helper_createPollRequestType(PollType.MANUAL_POLL);
+
+        // create a poll
+        CreatePollResultDto createPollResultDto = pollServiceBean.createPoll(pollRequestType, "TEST");
+        em.flush();
+
+        if(createPollResultDto.getSentPolls().size() == 0 && createPollResultDto.getUnsentPolls().size() == 0) {
+            Assert.fail();
+        }
     }
 
     @Test
@@ -56,7 +78,7 @@ public class PollServiceBeanIntTest extends TransactionalTests {
         try {
             System.setProperty(MESSAGE_PRODUCER_METHODS_FAIL, "true");
             PollRequestType pollRequestType = testPollHelper.createPollRequestType();
-            pollService.createPoll(pollRequestType, "TEST");
+            pollServiceBean.createPoll(pollRequestType, "TEST");
             Assert.fail();
         }
         catch(Throwable t){
@@ -70,14 +92,14 @@ public class PollServiceBeanIntTest extends TransactionalTests {
         Date latestRun = testPollHelper.getLatestRunDate();
         Date stopDate = testPollHelper.getStopDate();
 
-        int numberOfProgramB4 = pollService.getRunningProgramPolls().size();
+        int numberOfProgramB4 = pollServiceBean.getRunningProgramPolls().size();
 
         String mobileTerminalSerialNumber = testPollHelper.createSerialNumber();
         PollProgram pollProgram = testPollHelper.createPollProgramHelper(mobileTerminalSerialNumber, startDate, stopDate, latestRun);
 
         pollProgramDao.createPollProgram(pollProgram);
 
-        List<PollDto> runningProgramPolls = pollService.getRunningProgramPolls();
+        List<PollDto> runningProgramPolls = pollServiceBean.getRunningProgramPolls();
         assertNotNull(runningProgramPolls);
         assertEquals(numberOfProgramB4 + 1, runningProgramPolls.size());
     }
@@ -96,7 +118,7 @@ public class PollServiceBeanIntTest extends TransactionalTests {
 
         pollProgramDao.createPollProgram(pollProgram);
 
-        PollResponseType responseType = pollService.startProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
+        PollResponseType responseType = pollServiceBean.startProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
         List<PollAttribute> attributes = responseType.getAttributes();
 
         boolean ok = false;
@@ -108,6 +130,34 @@ public class PollServiceBeanIntTest extends TransactionalTests {
         }
         assertTrue(ok);
         assertNotNull(responseType);
+    }
+
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void startProgramPoll_FromMPSBIT() throws Exception {
+
+        // we want to be able to tamper with the dates for proper test coverage
+        Date startDate = getStartDate();
+        Date latestRun = getLatestRunDate();
+        Date stopDate = getStopDate();
+
+        String username = "TEST";
+
+        String connectId = UUID.randomUUID().toString();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(connectId, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+        UUID guid = pollProgram.getId();
+
+        //PollDto startedProgramPoll = mappedPollService.startProgramPoll(guid.toString(), username);
+        PollResponseType pollResponse = pollServiceBean.startProgramPoll(guid.toString(), username);
+        PollDto startedProgramPoll = PollMapper.mapPoll(pollResponse);
+        assertNotNull(startedProgramPoll);
+
+        List<PollValue> values = startedProgramPoll.getValue();
+        boolean found = validatePollKeyValue(values, PollKey.PROGRAM_RUNNING, "true");
+        assertTrue(found);
     }
 
     @Test
@@ -124,7 +174,7 @@ public class PollServiceBeanIntTest extends TransactionalTests {
 
         pollProgramDao.createPollProgram(pollProgram);
 
-        PollResponseType responseType = pollService.stopProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
+        PollResponseType responseType = pollServiceBean.stopProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
         List<PollAttribute> attributes = responseType.getAttributes();
 
         boolean ok = false;
@@ -136,6 +186,40 @@ public class PollServiceBeanIntTest extends TransactionalTests {
         }
         assertTrue(ok);
         assertNotNull(responseType);
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void stopProgramPoll_FromMPSBIT() throws Exception {
+
+//        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+        // we want to be able to tamper with the dates for proper test coverage
+        Date startDate = getStartDate();
+        Date latestRun = getLatestRunDate();
+        Date stopDate = getStopDate();
+
+        String username = "TEST";
+
+        String connectId = UUID.randomUUID().toString();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(connectId, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+        UUID guid = pollProgram.getId();
+
+        //PollDto startedProgramPoll = mappedPollService.startProgramPoll(String.valueOf(guid), username);
+        PollResponseType pollResponse = pollServiceBean.startProgramPoll(guid.toString(), username);
+        PollDto startedProgramPoll = PollMapper.mapPoll(pollResponse);
+        assertNotNull(startedProgramPoll);
+
+        //PollDto stoppedProgramPoll = mappedPollService.stopProgramPoll(String.valueOf(guid), username);
+        pollResponse = pollServiceBean.stopProgramPoll(String.valueOf(guid), username);
+        PollDto stoppedProgramPoll = PollMapper.mapPoll(pollResponse);
+        assertNotNull(stoppedProgramPoll);
+
+        List<PollValue> values = stoppedProgramPoll.getValue();
+        boolean found = validatePollKeyValue(values, PollKey.PROGRAM_RUNNING, "false");
+        assertTrue(found);
     }
 
     @Test
@@ -151,7 +235,7 @@ public class PollServiceBeanIntTest extends TransactionalTests {
 
         pollProgramDao.createPollProgram(pollProgram);
 
-        PollResponseType responseType = pollService.inactivateProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
+        PollResponseType responseType = pollServiceBean.inactivateProgramPoll(pollProgram.getId().toString(), pollProgram.getUpdatedBy());
         List<PollAttribute> attributes = responseType.getAttributes();
 
         boolean ok = false;
@@ -163,6 +247,44 @@ public class PollServiceBeanIntTest extends TransactionalTests {
         }
         assertTrue(ok);
         assertNotNull(responseType);
+    }
+
+    @Test
+    @OperateOnDeployment("normal")   //TODO: Move to PollServiceBeanTest
+    public void inactivateProgramPoll_FromMPSBIT() throws Exception {
+
+//        System.setProperty(MessageProducerBean.MESSAGE_PRODUCER_METHODS_FAIL, "false");
+
+        // we want to be able to tamper with the dates for proper test coverage
+        Date startDate = getStartDate();
+        Date latestRun = getLatestRunDate();
+        Date stopDate = getStopDate();
+
+        String username = "TEST";
+
+        String connectId = UUID.randomUUID().toString();
+        PollProgram pollProgram = testPollHelper.createPollProgramHelper(connectId, startDate, stopDate, latestRun);
+
+        pollProgramDao.createPollProgram(pollProgram);
+        UUID guid = pollProgram.getId();
+
+        //PollDto startedProgramPoll = mappedPollService.startProgramPoll(String.valueOf(guid), username);
+        PollResponseType pollResponse = pollServiceBean.startProgramPoll(guid.toString(), username);
+        PollDto startedProgramPoll = PollMapper.mapPoll(pollResponse);
+        assertNotNull(startedProgramPoll);
+
+        List<PollValue> values = startedProgramPoll.getValue();
+        boolean isRunning = validatePollKeyValue(values, PollKey.PROGRAM_RUNNING, "true");
+        assertTrue(isRunning);
+
+        //PollDto inactivatedProgramPoll = mappedPollService.inactivateProgramPoll(String.valueOf(guid), username);
+        pollResponse = pollServiceBean.inactivateProgramPoll(String.valueOf(guid), username);
+        PollDto inactivatedProgramPoll = PollMapper.mapPoll(pollResponse);
+        assertNotNull(inactivatedProgramPoll);
+
+        List<PollValue> values1 = inactivatedProgramPoll.getValue();
+        boolean isStopped = validatePollKeyValue(values1, PollKey.PROGRAM_RUNNING, "false");
+        assertTrue(isStopped);
     }
 
     @Test
@@ -179,7 +301,7 @@ public class PollServiceBeanIntTest extends TransactionalTests {
 
         pollProgramDao.createPollProgram(pollProgram);
 
-        List<PollResponseType> responseTypeList = pollService.timer();
+        List<PollResponseType> responseTypeList = pollServiceBean.timer();
 
         assertNotNull(responseTypeList);
         assertEquals(1, responseTypeList.size());
@@ -195,5 +317,111 @@ public class PollServiceBeanIntTest extends TransactionalTests {
             }
         }
         assertTrue(ok);
+    }
+
+
+    @Test
+    @OperateOnDeployment("normal")    //TODO: Move to PollServiceBeanTest
+    public void startProgramPoll_ShouldFailWithNullAsPollId() throws Exception {
+
+        try {
+            pollServiceBean.startProgramPoll(null, "TEST");
+            Assert.fail();
+        }
+        catch(EJBTransactionRolledbackException e){
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void stopProgramPoll_ShouldFailWithNullAsPollId() throws Exception {
+
+        thrown.expect(EJBTransactionRolledbackException.class);
+//        thrown.expectMessage("No poll id given");
+
+        pollServiceBean.stopProgramPoll(null, "TEST");
+    }
+
+
+    private PollRequestType helper_createPollRequestType(PollType pollType) throws Exception {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2015);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+        String startDate = format.format(cal.getTime());
+        cal.set(Calendar.YEAR, 2020);
+        String endDate = format.format(cal.getTime());
+
+        PollRequestType prt = new PollRequestType();
+        prt.setComment("aComment" + UUID.randomUUID().toString());
+        prt.setUserName("TEST");
+        prt.setPollType(pollType);
+        PollMobileTerminal pollMobileTerminal = helper_createPollMobileTerminal();
+        prt.getMobileTerminals().add(pollMobileTerminal);
+
+        PollAttribute psStart = new PollAttribute();
+        PollAttribute psEnd = new PollAttribute();
+        PollAttribute psFreq = new PollAttribute();
+
+        psStart.setKey(PollAttributeType.START_DATE);
+        psStart.setValue(startDate);
+        prt.getAttributes().add(psStart);
+
+        psEnd.setKey(PollAttributeType.END_DATE);
+        psEnd.setValue(endDate);
+        prt.getAttributes().add(psEnd);
+
+        psFreq.setKey(PollAttributeType.FREQUENCY);
+        psFreq.setValue("300000");
+        prt.getAttributes().add(psFreq);
+
+        return prt;
+    }
+
+    private PollMobileTerminal helper_createPollMobileTerminal() throws Exception {
+
+        UUID connectId = UUID.randomUUID();
+
+        MobileTerminal mobileTerminal = testPollHelper.createMobileTerminal(connectId.toString());
+        PollMobileTerminal pmt = new PollMobileTerminal();
+        pmt.setConnectId(connectId.toString());
+        pmt.setMobileTerminalId(mobileTerminal.getId().toString());
+
+        Set<Channel> channels = mobileTerminal.getChannels();
+        Channel channel = channels.iterator().next();
+        UUID channelId = channel.getId();
+        pmt.setComChannelId(channelId.toString());
+        return pmt;
+    }
+
+    private boolean validatePollKeyValue(List<PollValue> values, PollKey key, String value) {
+        for(PollValue v : values) {
+            PollKey pollKey = v.getKey();
+            if(pollKey.equals(key) && v.getValue().equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Date getStartDate() {
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        int startYear = 1999;
+        cal.set(Calendar.YEAR, startYear);
+        return cal.getTime();
+    }
+
+    private Date getLatestRunDate() {
+        cal.set(Calendar.DAY_OF_MONTH, 20);
+        int latestRunYear = 2017;
+        cal.set(Calendar.YEAR, latestRunYear);
+        return cal.getTime();
+    }
+
+    private Date getStopDate() {
+        cal.set(Calendar.DAY_OF_MONTH, 28);
+        cal.set(Calendar.YEAR, 2019);
+        return cal.getTime();
     }
 }
