8df1a66d46ac44bb18c7c2506dfba1f33d70075e
Kasim Gul <kasimgul@gmail.com>
2019-09-23 08:51:05 +0200

    Fetching correct mt revision by asset list call

--- service/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
@@ -135,9 +135,16 @@ public class AssetServiceBean implements AssetService {
 
         List<Asset> assetEntityList = assetDao.getAssetListSearchPaginated(page, listSize, searchFields, dynamic, includeInactivated);
         // force to load children. FetchType.EAGER didn't work.
+        List<MobileTerminal> terminalList = new ArrayList<>();
         assetEntityList.forEach(asset -> {
             if(asset.getMobileTerminals() != null) {
-                asset.getMobileTerminals().size();
+                List<MobileTerminal> terminals = asset.getMobileTerminals();
+                terminals.forEach(mt -> {
+                    MobileTerminal byId = mobileTerminalService.getMobileTerminalEntityById(mt.getId());
+                    terminalList.add(byId);
+                });
+                asset.getMobileTerminals().clear();
+                asset.getMobileTerminals().addAll(terminalList);
             }
         });
         AssetListResponse listAssetResponse = new AssetListResponse();
