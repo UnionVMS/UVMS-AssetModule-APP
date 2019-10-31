deadb0a80b975a0789893f74bfcd43b90b9b1859
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-10-31 10:11:00 +0100

    Introduced the event stream

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/asset/message/EventStreamSender.java
@@ -0,0 +1,88 @@
+package eu.europa.ec.fisheries.uvms.asset.message;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import eu.europa.ec.fisheries.uvms.asset.domain.entity.Asset;
+import eu.europa.ec.fisheries.uvms.asset.dto.AssetMergeInfo;
+import eu.europa.ec.fisheries.uvms.asset.dto.MicroAsset;
+import eu.europa.ec.fisheries.uvms.asset.message.event.UpdatedAssetEvent;
+import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
+import eu.europa.ec.fisheries.uvms.commons.message.context.MappedDiagnosticContext;
+import eu.europa.ec.fisheries.uvms.rest.security.RequiresFeature;
+import eu.europa.ec.fisheries.uvms.rest.security.UnionVMSFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.TransactionPhase;
+import javax.inject.Inject;
+import javax.jms.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.sse.OutboundSseEvent;
+import javax.ws.rs.sse.Sse;
+import javax.ws.rs.sse.SseBroadcaster;
+import javax.ws.rs.sse.SseEventSink;
+
+@Stateless
+public class EventStreamSender {
+
+    private final static Logger LOG = LoggerFactory.getLogger(EventStreamSender.class);
+
+    @Resource(mappedName = "java:/" + MessageConstants.EVENT_STREAM_TOPIC)
+    private Destination destination;
+
+    @Inject
+    @JMSConnectionFactory("java:/ConnectionFactory")
+    JMSContext context;
+
+    private ObjectMapper om = new ObjectMapper();
+
+    @PostConstruct
+    public void init() {
+        om.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        om.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    public void updatedAsset(@Observes(during = TransactionPhase.AFTER_SUCCESS) @UpdatedAssetEvent Asset asset){
+        try {
+            if (asset != null) {
+                MicroAsset micro = new MicroAsset(asset.getId(), asset.getFlagStateCode(), asset.getName(), asset.getVesselType(), asset.getIrcs(), asset.getCfr(), asset.getExternalMarking(), asset.getLengthOverAll());
+                String outgoingJson = om.writeValueAsString(micro);
+                sendMessageOnEventStream(outgoingJson, "Updated Asset");
+            }
+        }catch (Exception e){
+            LOG.error("Error while sending message on event stream: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void mergeAsset(@Observes(during = TransactionPhase.AFTER_SUCCESS) @UpdatedAssetEvent AssetMergeInfo mergeInfo){
+        try {
+            if (mergeInfo != null) {
+                String outgoingJson = om.writeValueAsString(mergeInfo);
+                sendMessageOnEventStream(outgoingJson, "Merged Asset");
+            }
+        }catch (Exception e){
+            LOG.error("Error while sending message on event stream: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendMessageOnEventStream(String outgoingJson, String eventName) throws JMSException {
+        TextMessage message = this.context.createTextMessage(outgoingJson);
+        message.setStringProperty(MessageConstants.EVENT_STREAM_EVENT, eventName);
+        message.setStringProperty(MessageConstants.EVENT_STREAM_SUBSCRIBER_LIST, null);
+        MappedDiagnosticContext.addThreadMappedDiagnosticContextToMessageProperties(message);
+
+        context.createProducer().setDeliveryMode(1).setTimeToLive(5000L).send(destination, message);
+    }
+}
---
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/asset/message/EventStreamSenderTest.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.europa.ec.fisheries.uvms.asset.message;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import eu.europa.ec.fisheries.uvms.asset.domain.dao.AssetDao;
+import eu.europa.ec.fisheries.uvms.asset.domain.entity.AssetRemapMapping;
+import eu.europa.ec.fisheries.uvms.asset.dto.AssetMergeInfo;
+import eu.europa.ec.fisheries.uvms.asset.dto.MicroAsset;
+import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
+import eu.europa.ec.fisheries.uvms.mobileterminal.timer.AssetRemapTask;
+import eu.europa.ec.fisheries.uvms.tests.BuildAssetServiceDeployment;
+import eu.europa.ec.fisheries.uvms.tests.asset.service.arquillian.arquillian.AssetRemapTest;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jem
+ */
+@RunWith(Arquillian.class)
+public class EventStreamSenderTest extends BuildAssetServiceDeployment {
+    
+    private final static Logger LOG = LoggerFactory.getLogger(EventStreamSenderTest.class);
+
+    @Resource(mappedName = "java:/ConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Inject
+    AssetDao assetDao;
+
+    @Inject
+    AssetRemapTask assetRemapTask;
+
+    JMSHelper jmsHelper;
+    MessageConsumer subscriber;
+    Topic eventBus;
+    Session session;
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @PostConstruct
+    public void init() {
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+    }
+    @Before
+    public void cleanJMS() {
+        jmsHelper = new JMSHelper();
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void createAssetCheckEventStream() throws Exception {
+        eu.europa.ec.fisheries.wsdl.asset.types.Asset asset = AssetTestHelper.createBasicAsset();
+        jmsHelper.upsertAsset(asset);
+        registerSubscriber();
+        TextMessage message = (TextMessage)listenOnEventStream(5000l);
+        assertNotNull(message);
+
+        assertEquals("Updated Asset", message.getStringProperty(MessageConstants.EVENT_STREAM_EVENT));
+        assertNull(message.getStringProperty(MessageConstants.EVENT_STREAM_SUBSCRIBER_LIST));
+        MicroAsset micro = mapper.readValue(message.getText(), MicroAsset.class);
+        assertNotNull(micro);
+
+        assertEquals(asset.getIrcs(), micro.getIrcs());
+        assertEquals(asset.getCfr(), micro.getCfr());
+        assertEquals(asset.getName(), micro.getAssetName());
+
+    }
+
+    @Test
+    @OperateOnDeployment("normal")
+    public void checkThatMergeMessageComesOnSSEStreamTest() throws Exception{
+        eu.europa.ec.fisheries.wsdl.asset.types.Asset asset1 = AssetTestHelper.createBasicAsset();
+        jmsHelper.upsertAsset(asset1);
+        registerSubscriber();
+        TextMessage message = (TextMessage)listenOnEventStream(5000l);
+        assertNotNull(message);
+        assertEquals("Updated Asset", message.getStringProperty(MessageConstants.EVENT_STREAM_EVENT));
+        MicroAsset oldAsset = mapper.readValue(message.getText(), MicroAsset.class);
+
+        eu.europa.ec.fisheries.wsdl.asset.types.Asset asset2 = AssetTestHelper.createBasicAsset();
+        jmsHelper.upsertAsset(asset2);
+        registerSubscriber();
+        message = (TextMessage)listenOnEventStream(5000l);
+        assertNotNull(message);
+        assertEquals("Updated Asset", message.getStringProperty(MessageConstants.EVENT_STREAM_EVENT));
+        MicroAsset newAsset = mapper.readValue(message.getText(), MicroAsset.class);
+
+        AssetRemapMapping assetRemapMapping = new AssetRemapMapping();
+        assetRemapMapping.setOldAssetId(UUID.fromString(oldAsset.getAssetId()));
+        assetRemapMapping.setNewAssetId(UUID.fromString(newAsset.getAssetId()));
+        assetRemapMapping.setCreatedDate(Instant.now().minus(4, ChronoUnit.HOURS));
+
+        assetRemapMapping = assetDao.createAssetRemapMapping(assetRemapMapping);
+
+
+        registerSubscriber();
+        assetRemapTask.remap();
+        message = (TextMessage)listenOnEventStream(5000l);
+        assertNotNull(message);
+        assertEquals("Merged Asset", message.getStringProperty(MessageConstants.EVENT_STREAM_EVENT));
+
+        AssetMergeInfo mergeInfo = mapper.readValue(message.getText(), AssetMergeInfo.class);
+        assertEquals(oldAsset.getAssetId(), mergeInfo.getOldAssetId());
+        assertEquals(newAsset.getAssetId(), mergeInfo.getNewAssetId());
+
+    }
+
+
+    public void registerSubscriber() throws Exception {
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        eventBus = session.createTopic("EventStream");
+        subscriber = session.createConsumer(eventBus, null, true);
+    }
+
+
+    public Message listenOnEventStream(Long timeoutInMillis) throws Exception {
+
+        try {
+            return subscriber.receive(timeoutInMillis);
+        } finally {
+            subscriber.close();
+        }
+    }
+    
+}
