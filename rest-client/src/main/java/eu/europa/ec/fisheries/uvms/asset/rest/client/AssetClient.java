d0cd654d3b0d5f6dca80980a195d81eaee43ef78
kwstaspgr <constantinos.papatheodorou@sword-group.com>
2020-10-20 11:50:20 +0300

    [UNIONVMS-4858] expose new endpoints for the rest of the rules-asset communication dependencies

--- rest-client/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/client/AssetClient.java
+++ rest-client/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/client/AssetClient.java
@@ -11,11 +11,20 @@ package eu.europa.ec.fisheries.uvms.asset.rest.client;
 
 import java.util.List;
 
+import eu.europa.ec.fisheries.wsdl.asset.group.AssetGroup;
 import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
+import eu.europa.ec.fisheries.wsdl.asset.types.AssetId;
+import eu.europa.ec.fisheries.wsdl.asset.types.AssetListQuery;
+import eu.europa.ec.fisheries.wsdl.asset.types.BatchAssetListResponseElement;
 
 public interface AssetClient {
     List<Asset> findHistoryOfAssetByCfr(String cfr);
 
-    List<Asset> findHistoryOfAssetBy(String reportDate, String cfr, String regCountry, String ircs, String extMark, String iccat);
+    List<Asset> findHistoryOfAssetBy(String reportDate, String cfr, String regCountry, String ircs, String extMark, String iccat, String uvi);
 
+    List<BatchAssetListResponseElement> getAssetListBatch(List<AssetListQuery> assetBatchRequest);
+
+    Asset getAsset(AssetId assetId);
+
+    List<AssetGroup> getAssetGroupListByAssetGuid(String assetGuid);
 }
--- rest/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/error/AssetError.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/asset/rest/error/AssetError.java
@@ -2,6 +2,7 @@ package eu.europa.ec.fisheries.uvms.asset.rest.error;
 
 public enum AssetError {
     SQL_ERROR(100, "SQL_ERROR", "Error occurred in query execution"),
+    ASSET_ERROR(1700,"ASSET_ERROR", "Error occurred when getting asset"),
     UNKNOWN_ERROR(1000, "UNKNOWN_ERROR", "Unknown error ocurred");
 
     public int getCode() {
