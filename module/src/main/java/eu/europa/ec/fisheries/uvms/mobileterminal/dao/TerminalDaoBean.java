4bffd8805fb93427ac78891fbb53eed453e37266
johan <johan.soderblom@havochvatten.se>
2021-03-25 09:00:09 +0100

    add mob id on JOIN

--- module/src/main/java/eu/europa/ec/fisheries/uvms/mobileterminal/dao/TerminalDaoBean.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/mobileterminal/dao/TerminalDaoBean.java
@@ -419,9 +419,9 @@ public class TerminalDaoBean {
         Query q = em.createNativeQuery("SELECT DESTINCT c.dnid, c.member_number,\n" +
                 " c.com_channel_name, m.serial_no,\n" +
                 " m.satellite_number, a.national_id,\n" +
-                "c.start_date, c.end_date\n" + 
+                " c.start_date, c.end_date\n" + 
                 "FROM asset.channel_aud c\n" + 
-                "JOIN asset.mobileterminal_aud m ON c.rev = m.rev\n" + 
+                "JOIN asset.mobileterminal_aud m ON c.mobterm_id = m.id AND c.rev = m.rev \n" + 
                 "JOIN asset.asset_aud a ON m.asset_id = a.id \n" + 
                 "WHERE a.updatetime = (SELECT MAX(aud.updatetime)\n" + 
                 "                      FROM asset.asset_aud aud\n" + 
@@ -432,13 +432,14 @@ public class TerminalDaoBean {
                 "AND a.national_id IS NOT NULL\n" +
                 "UNION\n" +
                 "SELECT c.dnid, c.member_number, c.com_channel_name,\n" + 
-                "m.serial_no, m.satellite_number, a.national_id, c.end_date, c.start_date\n" +
+                " m.serial_no, m.satellite_number, a.national_id, c.end_date, c.start_date\n" +
                 "FROM asset.channel c \n" +
                 "JOIN asset.mobileterminal m ON c.mobterm_id = m.id \n" +
                 "JOIN asset.asset a ON m.asset_id = a.id \n" +
                 "AND c.chan_conf = false\n" +
                 "AND c.chan_poll = false\n" +
                 "AND a.national_id IS NOT NULL");
+        
         List<Object[]> vmsBillingObject = q.getResultList();
         List<VmsBillingDto> vmsBillingListDao = new ArrayList<>();
         
