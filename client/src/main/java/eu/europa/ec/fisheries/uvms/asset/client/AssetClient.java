f895d47a190c9e2bb298c7bb61d1a51d83ec75d8
johan <johan.soderblom@havochvatten.se>
2021-08-31 08:59:43 +0200

    change endpoint name

--- client/src/main/java/eu/europa/ec/fisheries/uvms/asset/client/AssetClient.java
+++ client/src/main/java/eu/europa/ec/fisheries/uvms/asset/client/AssetClient.java
@@ -343,7 +343,7 @@ public class AssetClient {
     
     public String getAssetList(List<String> assetIdList){
         Response response = webTarget
-                .path("microAssets")
+                .path("assetListFromListOfIDs")
                 .request(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.AUTHORIZATION, tokenHandler.createAndFetchToken("user"))
