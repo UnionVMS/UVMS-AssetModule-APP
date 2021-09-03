7edcb856de04473f508fe0e99af3dd63734fd89c
Tommi Ahonen <72734604+Hamburgaren@users.noreply.github.com>
2021-09-03 15:37:48 +0200

    Fix SonarCloud code smells: Utility classes should not have public constructors (implicit in this case).

--- model/src/main/java/eu/europa/ec/fisheries/uvms/asset/model/mapper/AssetModuleRequestMapper.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/asset/model/mapper/AssetModuleRequestMapper.java
@@ -23,6 +23,8 @@ import java.util.Date;
 
 public class AssetModuleRequestMapper {
 
+    private AssetModuleRequestMapper() { }
+
     private final static Logger LOG = LoggerFactory.getLogger(AssetModuleRequestMapper.class);
 
     public static String createGetAssetModuleRequest(String value, AssetIdType type) throws AssetException {
