3b31254da65de8b09a511b49f4c1b832bb0418b8
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-09-07 15:29:58 +0200

    Code smell fix
    added a private constructor to hide the implicit one

--- module/src/main/java/eu/europa/ec/fisheries/uvms/rest/asset/Constant.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/rest/asset/Constant.java
@@ -13,6 +13,8 @@ package eu.europa.ec.fisheries.uvms.rest.asset;
 
 public class Constant {
 
+    private Constant() {}
+
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
     public static final String ACCESS_CONTROL_ALLOW_METHODS_ALL = "*";
 
