384478e2bb2da281471c65f8f53b7be49a674e65
thohar2001 <thomas.haraldsson5@gmail.com>
2021-09-13 13:58:29 +0200

    Code smell fix: Replace this if-then-else statement by a single return statement
    Code smell fix: Replace this if-then-else statement by a single return statement. Made boolean variable "empty", set to true by default but if it's not empty it is set to false and then the value is returned.

--- module/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
@@ -186,10 +186,11 @@ public class AssetServiceBean {
     }
 
     private static boolean isEmpty(String text) {
-        if (text == null || text.isEmpty())
-            return true;
-        else
-            return false;
+        boolean emptyOrNot = true;
+        if (text != null || !text.isEmpty())
+            emptyOrNot = false;
+
+        return emptyOrNot;
     }
 
     private void checkIdentifierNullValues(Asset a) {
