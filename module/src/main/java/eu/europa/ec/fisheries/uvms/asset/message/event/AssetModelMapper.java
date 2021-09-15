b1044b373372655adc5b9b00ac6987060ae8a5b7
tommiahonen <tommi.t.ahonen@gmail.com>
2021-09-15 09:17:02 +0200

    Fix SonarQube code smells: Reducer cognitive complexity of method by moving code to new static method.

--- module/src/main/java/eu/europa/ec/fisheries/uvms/asset/message/event/AssetModelMapper.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/asset/message/event/AssetModelMapper.java
@@ -175,15 +175,7 @@ public class AssetModelMapper {
         assetModel.setProducer(prodOrg);
         
         Collection<Note> notes = assetService.getNotesForAsset(assetEntity.getId()).values();
-        for (Note note : notes) {
-            AssetNotes assetNote = new AssetNotes();
-            assetNote.setId(note.getId().toString());
-            if (note.getCreatedOn() != null) {
-                assetNote.setDate(DateUtils.dateToEpochMilliseconds(note.getCreatedOn()));
-            }
-            assetNote.setNotes(note.getNoteText());
-            assetModel.getNotes().add(assetNote);
-        }
+        addNotesToAsset(notes, assetModel);
 
         List<ContactInfo> contacts = assetService.getContactInfoForAsset(assetEntity.getId());
         for (ContactInfo contactInfo : contacts) {
@@ -205,6 +197,23 @@ public class AssetModelMapper {
         assetModel.setGfcm(assetEntity.getGfcm());
         
         return assetModel;
+    }
+
+    /**
+     * Add a Collection of notes to an asset model.
+     * @param notes Notes to be added.
+     * @param assetModel Notes will be added to this asset model.
+     */
+    private static void addNotesToAsset(Collection<Note> notes, eu.europa.ec.fisheries.wsdl.asset.types.Asset assetModel) {
+        for (Note note : notes) {
+            AssetNotes assetNote = new AssetNotes();
+            assetNote.setId(note.getId().toString());
+            if (note.getCreatedOn() != null) {
+                assetNote.setDate(DateUtils.dateToEpochMilliseconds(note.getCreatedOn()));
+            }
+            assetNote.setNotes(note.getNoteText());
+            assetModel.getNotes().add(assetNote);
+        }
     }
 
     private EventCode getEventCode(Asset assetEntity) {
