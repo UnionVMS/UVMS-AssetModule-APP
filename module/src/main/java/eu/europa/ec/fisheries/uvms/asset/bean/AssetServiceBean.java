6fa11c0c7501d8f0ba457999298bc03c19778c4f
peerik <peter.eriksson@havochvatten.se>
2021-09-16 11:09:46 +0200

    Remove whitespace from AIS ircs in comparison

--- module/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/asset/bean/AssetServiceBean.java
@@ -715,7 +715,7 @@ public class AssetServiceBean {
             shouldUpdate = true;
             assetFromDB.setMmsi(assetFromAIS.getMmsi());
         }
-        if ((assetFromDB.getIrcs() == null || !assetFromDB.getIrcs().equals(assetFromAIS.getIrcs())) && (assetFromAIS.getIrcs() != null)) {
+        if ((assetFromDB.getIrcs() == null || !assetFromDB.getIrcs().equals(assetFromAIS.getIrcs().replace(" ", ""))) && (assetFromAIS.getIrcs() != null)) {
             shouldUpdate = true;
             assetFromDB.setIrcs(assetFromAIS.getIrcs().replace(" ", ""));
         }
