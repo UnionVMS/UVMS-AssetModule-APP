f6b3b2bc472a1e0c72f0756c727b24ed7a55f937
Mehrdadbadeie <72803122+Mehrdadbadeie@users.noreply.github.com>
2021-09-22 11:34:26 +0200

    refactoring
    added separeted methods for every junit test instead all in one giant method

--- module/src/test/java/eu/europa/ec/fisheries/uvms/asset/dto/MicroAssetTest.java
+++ module/src/test/java/eu/europa/ec/fisheries/uvms/asset/dto/MicroAssetTest.java
@@ -9,39 +9,68 @@ import static org.junit.Assert.*;
 
 public class MicroAssetTest {
 
+    MicroAsset ma = new MicroAsset(UUID.randomUUID(), "flagstate", "assetname", "vesseltype", "ircs",
+            "cfr", "externalmarking", 15.35,false);
+
     @Test
     public void createMicroAssetTest () {
-        MicroAsset ma = new MicroAsset(UUID.randomUUID(), "flagstate", "assetname", "vesseltype", "ircs",
-                                        "cfr", "externalmarking", 15.35,false);
-
         MicroAsset ma2 = new MicroAsset();
         assertNotNull(ma2);
+    }
 
+    @Test
+    public void setAndGetAssetIdTest () {
         ma.setAssetId(UUID.randomUUID());
         assertNotNull(ma.getAssetId());
+    }
 
+    @Test
+    public void setAndGetFlagstateTest () {
         ma.setFlagstate("testflagstate");
         assertEquals("testflagstate", ma.getFlagstate());
+    }
 
+    @Test
+    public void setAndGetAssetNameTest () {
         ma.setAssetName("testassetname");
         assertEquals("testassetname", ma.getAssetName());
+    }
 
+    @Test
+    public void setAndGetVesselTypeTest () {
         ma.setVesselType("optimistjolle");
         assertEquals("optimistjolle", ma.getVesselType());
+    }
 
+    @Test
+    public void setAndGetIrcsTest () {
         ma.setIrcs("testircs");
         assertEquals("testircs", ma.getIrcs());
+    }
 
+    @Test
+    public void setAndGetCfrTest () {
         ma.setCfr("testcfr");
         assertEquals("testcfr", ma.getCfr());
+    }
 
+    @Test
+    public void setAndGetExternalMarkingTest () {
         ma.setExternalMarking("testexternalmarking");
         assertEquals("testexternalmarking", ma.getExternalMarking());
+    }
 
+    @Test
+    public void setAndGetLengthOverAllTest () {
         ma.setLengthOverAll(16.45);
         assertEquals(16.45, ma.getLengthOverAll(), 0.0);
+    }
 
+    @Test
+    public void setAndGetHasLicenceTest () {
         ma.setHasLicence(true);
         assertEquals(true, ma.getHasLicence());
     }
+
+
 }
